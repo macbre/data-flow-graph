1b517eaefc778ee4d52709f25a891d6eac7e4013
macbre <maciej.brencz@gmail.com>
2017-12-18 23:08:44 +0100

    Use scapy library to parse pcap files

--- sources/pcap/pcap-to-data-flow.py
+++ sources/pcap/pcap-to-data-flow.py
@@ -1,36 +1,77 @@
 import logging
 import sys
 
-from pcapfile import savefile
-from pcapfile.protocols.linklayer import ethernet
-from pcapfile.protocols.network import ip
+from scapy.all import rdpcap
 
 
 logging.basicConfig(level=logging.INFO)
 
 
-def parse(f):
+def parse_redis_packet(packet):
+	(ip, raw) = packet
+
+	# ['*2\r', '$4\r', 'lpop\r', '$30\r', 'mq::elecena_products::messages']
+	try:
+		(_, _, cmd, _, arg) = str(raw).strip().split('\n')[:5]
+		# print(cmd, arg)
+
+		cmd = cmd.strip().lower()
+		arg = arg.lower()
+
+		if cmd == 'lpop':
+			# take from the queue
+			return '{source}\t{edge}\t{target}'.format(
+				target=ip.src,
+				edge=cmd,
+				source=arg
+			)
+		elif cmd == 'rpush':
+			# push the queue
+			return '{source}\t{edge}\t{target}'.format(
+				source=ip.src,
+				edge=cmd,
+				target=arg
+			)
+		else:
+			return None
+
+	except ValueError:
+		return None
+
+
+def parse(f, proto):
 	logger = logging.getLogger('pcap-to-data-flow')
-	logger.info('Reading %s ..', repr(f))
+	logger.info('Reading %s as %s proto ..', repr(f), proto)
 
 	# @see http://kisom.github.io/pypcapfile/
-	capfile = savefile.load_savefile(f, layers=0, verbose=False)
+	# @see https://stackoverflow.com/questions/42963343/reading-pcap-file-with-scapy
+	packets = rdpcap(f)
+	packets_count = len(packets)
 
-	logger.info('pcap file info: %s', repr(capfile).rstrip().replace('\n', ' / '))
+	logger.info('Packets read: %d / %s', packets_count, repr(packets))
+	logger.info('First one: %s', repr(packets[0]))
+	logger.info('Last one: %s', repr(packets[-1]))
 
-	for packet in capfile.packets:
-		try:
-			eth_frame = ethernet.Ethernet(packet.raw())
+	packets = [
+		(packet['IP'], packet['Raw'])
+		for packet in packets
+	]
 
-			logger.info(eth_frame)
-			ip_packet = ip.IP(eth_frame.payload)
+	# print(packets)
 
-			logger.info(ip_packet)
-		except AssertionError:
-			logger.error('Error handling a packet', exc_info=True)
-			raise
+	# protocol specific handling
+	if proto == 'redis':
+		packets = map(parse_redis_packet, packets)
+	else:
+		raise Exception('Unsupported proto: %s', proto)
+
+	# remove empty entries and duplicates
+	packets = filter(lambda x: x is not None, packets)
+	packets = set(packets)
+
+	print('# processed {} packets as {}'.format(packets_count, proto))
+	print('\n'.join(packets))
 
 
 if __name__ == "__main__":
-	with open(sys.argv[1], 'rb') as f:
-		parse(f)
+	parse(sys.argv[1], sys.argv[2])
