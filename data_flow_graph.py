f699907547b4b966d3705f7d8379c6b90ac1c6eb
macbre <maciej.brencz@gmail.com>
2018-04-30 20:02:07 +0200

    Add format_tsv_lines() helper

--- data_flow_graph.py
+++ data_flow_graph.py
@@ -24,6 +24,16 @@ def format_tsv_line(source, edge, target, value=None, metadata=None):
     ).rstrip(' \t')
 
 
+def format_tsv_lines(lines):
+    """
+    Render a set of data into a list of TSV-formatted lines
+
+    :type lines list[dict]
+    :rtype: list[str]
+    """
+    return [format_tsv_line(**line) for line in lines]
+
+
 def logs_map_and_reduce(logs, _map, _reduce):
     """
     :type logs str[]
--- test/test_tsv.py
+++ test/test_tsv.py
@@ -1,4 +1,4 @@
-from data_flow_graph import format_tsv_line
+from data_flow_graph import format_tsv_line, format_tsv_lines
 
 
 def test_format():
@@ -8,3 +8,22 @@ def test_format():
     assert format_tsv_line('foo', 'select', 'bar', value=0.12) == 'foo\tselect\tbar\t0.1200'
     assert format_tsv_line('foo', 'select', 'bar', value=0.12, metadata='QPS: 1234') == \
         'foo\tselect\tbar\t0.1200\tQPS: 1234'
+
+
+def test_format_lines():
+    lines = [
+        {
+            'source': 'foo',
+            'edge': 'select',
+            'target': 'bar',
+        },
+        {
+            'source': 'foo2',
+            'edge': 'select',
+            'target': 'bar',
+            'value': 0.5,
+            'metadata': 'test'
+        },
+    ]
+
+    assert '\n'.join(format_tsv_lines(lines)) == 'foo\tselect\tbar\nfoo2\tselect\tbar\t0.5000\ttest'
